d0da6f0bfb025e4696987b73809ee02516126822
Ololade Adebanjo <ololadeadebanjo7@gmail.com>
2022-07-03 03:56:24 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ * Description: print "\"Programming is like building a multilingual puzzle"*
+ * Return: Always 0 Success)
+ */
 
 int main(void)
 {
